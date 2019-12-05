d9b926b37e417ab356e454c0668b70e46dff48fd
Thiago Oliveira <braddock01@hotmail.com>
2019-12-05 10:01:32 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_prato = new System.Windows.Forms.TextBox();
+            this.txt_ingredientes = new System.Windows.Forms.TextBox();
+            this.txt_preco = new System.Windows.Forms.TextBox();
+            this.txt_sugestao = new System.Windows.Forms.TextBox();
+            this.cb_categoria = new System.Windows.Forms.ComboBox();
+            this.btn_cadastrar = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do prato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Categoria";
+          
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(335, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingredientes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(341, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Preço";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(274, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sugestão de acompanhamento";
+            
+            // 
+            // txt_prato
+            // 
+            this.txt_prato.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_prato.Location = new System.Drawing.Point(71, 89);
+            this.txt_prato.Name = "txt_prato";
+            this.txt_prato.Size = new System.Drawing.Size(241, 29);
+            this.txt_prato.TabIndex = 1;
+            // 
+            // txt_ingredientes
+            // 
+            this.txt_ingredientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ingredientes.Location = new System.Drawing.Point(339, 89);
+            this.txt_ingredientes.Name = "txt_ingredientes";
+            this.txt_ingredientes.Size = new System.Drawing.Size(241, 29);
+            this.txt_ingredientes.TabIndex = 2;
+            // 
+            // txt_preco
+            // 
+            this.txt_preco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_preco.Location = new System.Drawing.Point(376, 189);
+            this.txt_preco.Margin = new System.Windows.Forms.Padding(50, 3, 3, 3);
+            this.txt_preco.Name = "txt_preco";
+            this.txt_preco.Size = new System.Drawing.Size(204, 29);
+            this.txt_preco.TabIndex = 4;
+            
+            // 
+            // txt_sugestao
+            // 
+            this.txt_sugestao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sugestao.Location = new System.Drawing.Point(71, 300);
+            this.txt_sugestao.Multiline = true;
+            this.txt_sugestao.Name = "txt_sugestao";
+            this.txt_sugestao.Size = new System.Drawing.Size(509, 169);
+            this.txt_sugestao.TabIndex = 5;
+            // 
+            // cb_categoria
+            // 
+            this.cb_categoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_categoria.FormattingEnabled = true;
+            this.cb_categoria.Items.AddRange(new object[] {
+            "Bebidas",
+            "Salgados",
+            "Doces",
+            "Vegano"});
+            this.cb_categoria.Location = new System.Drawing.Point(71, 186);
+            this.cb_categoria.Name = "cb_categoria";
+            this.cb_categoria.Size = new System.Drawing.Size(241, 32);
+            this.cb_categoria.TabIndex = 3;
+            this.cb_categoria.Text = "Escolha uma opção";
+            // 
+            // btn_cadastrar
+            // 
+            this.btn_cadastrar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btn_cadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cadastrar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_cadastrar.Location = new System.Drawing.Point(71, 507);
+            this.btn_cadastrar.Name = "btn_cadastrar";
+            this.btn_cadastrar.Size = new System.Drawing.Size(150, 60);
+            this.btn_cadastrar.TabIndex = 6;
+            this.btn_cadastrar.Text = "Cadastrar";
+            this.btn_cadastrar.UseVisualStyleBackColor = false;
+            this.btn_cadastrar.Click += new System.EventHandler(this.btn_cadastrar_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.BackColor = System.Drawing.Color.IndianRed;
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_limpar.Location = new System.Drawing.Point(430, 507);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(150, 60);
+            this.btn_limpar.TabIndex = 7;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = false;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(341, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "R$";
+            
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_cadastrar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 604);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.btn_cadastrar);
+            this.Controls.Add(this.cb_categoria);
+            this.Controls.Add(this.txt_sugestao);
+            this.Controls.Add(this.txt_preco);
+            this.Controls.Add(this.txt_ingredientes);
+            this.Controls.Add(this.txt_prato);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cardápio 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_prato;
+        private System.Windows.Forms.TextBox txt_ingredientes;
+        private System.Windows.Forms.TextBox txt_preco;
+        private System.Windows.Forms.TextBox txt_sugestao;
+        private System.Windows.Forms.ComboBox cb_categoria;
+        private System.Windows.Forms.Button btn_cadastrar;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            // Declaração de variáveis
+            string prato, ingredientes, categoria, sugestao;
+            double preco;
+
+
+            // Entrada de dados
+            prato = txt_prato.Text;
+            ingredientes = txt_ingredientes.Text;
+            categoria = cb_categoria.Text;
+            sugestao = txt_sugestao.Text;
+            preco = double.Parse(txt_preco.Text);
+
+            /* MessageBox.Show(
+                prato +
+                "\n" + ingredientes +
+                "\n" + categoria +
+                "\n" + sugestao +
+                "\n" + preco,
+                "Cardápio"
+            );
+            */
+
+            // Caminho para criação do arquivo de texto
+            string path = System.Environment.GetEnvironmentVariable("USERPROFILE");
+
+            //objeto responsavel por gravar no arquivo criado acima
+            StreamWriter io = new StreamWriter(path + "\\desktop\\cardapio.txt", true);
+
+            //escrever no arquivo 1ª opçao
+            //io.WriteLine(prato + ";" + ingredientes + ";" + categoria + ";" + sugestao + ";" + preco);
+
+            //escrever no arquivo 2ª opçao
+            string valores = string.Join(";", prato, ingredientes, categoria, sugestao, preco);
+            io.WriteLine(valores);
+
+            //fecha o arquivo para escrita
+            io.Close();
+            MessageBox.Show("Registro Cadastrado com Sucesso!", 
+                            "Gravando ...", 
+                            MessageBoxButtons.OK, 
+                            MessageBoxIcon.Information);
+                       
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            //txt_ingredientes.Clear();
+            //txt_prato.Clear();
+            //txt_preco.Clear();
+            //txt_sugestao.Clear();
+            //cb_categoria.Text = "Escolha uma opção";
+
+            //Outra maneira + smart ###################
+            foreach(Control c in this.Controls){
+                if(c is TextBox)
+                    (c as TextBox).Clear();
+                if (c is ComboBox)
+                    (c as ComboBox).Text = "Escolha uma opção";
+               
+            }
+            //#########################################
+
+            //define cursor no 1º campo
+            txt_prato.Focus();
+        }
+    }
+}
